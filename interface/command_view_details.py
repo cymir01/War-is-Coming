f9c085db34c0b5016d808cae8ef1968bc9afbe14
cymir01 <cynthiamiranda401@gmail.com>
2025-12-08 13:47:49 -0500

    improved command_view_details function

--- interface/command_view_details.py
+++ interface/command_view_details.py
@@ -1,14 +1,14 @@
-from date_validation import overlap
-from date_validation import validate_datetime_input
 from rich.console import Console
 from rich.table import Table
-from rich.panel import Panel
-from rich.prompt import Prompt
 from rich.text import Text
-from core import add_event, list_events
-from rich.prompt import Confirm
-from interface.command_add_event import command_add
-from interface.command_view_details import command_view_details
+from core import list_events
+
+#!Bug Report: (revisar por que ocurre el error)
+# cymir@cymir-ThinkPad-T440p:~/00 Pojects/War-is-Coming$ /bin/python3 "/home/cymir/00 Pojects/War-is-Coming/interface/command_view_details.py"
+# Traceback (most recent call last):
+#   File "/home/cymir/00 Pojects/War-is-Coming/interface/command_view_details.py", line 4, in <module>
+#     from core import list_events
+# ModuleNotFoundError: No module named 'core'
 
 console = Console()
 
@@ -16,12 +16,14 @@ console = Console()
 def command_view_details():
     events = list_events()
     cmd = console.input("Introduzca el número del evento para mostrar los detalles")
-    table = Table(title="Detalles del evento")
+    id = events[cmd] #!ejecutar y ver si esto funciona
+    table = Table(title=f"Detalles del evento {id}")
     table.add_column("ID", style="cyan")
     table.add_column("Descripción", style="magenta")
     table.add_column("Estado", style="green")
 
-    for id, event in events.items():
-        if cmd == id:
-            table.add_row(str(id), event['desc'], events['estado'])
+    # for id, event in events.items():
+    #     if cmd == id:
+    #         table.add_row(str(id), event['desc'], events['estado'])
     
+command_view_details()
