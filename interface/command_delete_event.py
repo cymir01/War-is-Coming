a812a20c3e57071b87d0330e4db858361f840949
cymir01 <cynthiamiranda401@gmail.com>
2025-12-09 18:51:23 -0500

    built command_delete function

--- interface/command_delete_event.py
+++ interface/command_delete_event.py
@@ -1,3 +1,15 @@
+from rich.console import Console
 
+#!agregar la funcion de liberar los recursos que usa el evento
 def command_delete():
-    pass
+    events = {'name': "nombre"}
+    while True:
+        console = Console()
+        cmd = console.print("Introduzca el nombre del evento que desea eliminar o presione  ")
+        if cmd.lower() == 's':
+            break
+        if cmd == events[name]:
+            del events[name]
+            break
+        else:
+            return "El evento no existe"
